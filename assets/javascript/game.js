fb39357e63010c460d9edcc72d455418b1da53f1
FrankV <frank@sabertechnology.com>
2018-12-03 19:31:51 -0500

    Game logic working

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en-us">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Word Guess Game</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+  <script defer src="assets/javascript/main.js"></script>
+</head>
+
+<body>
+        
+
+
+
+<script type="text/javascript">
+
+
+
+
+consol.log("this file works");
+
+
+</script>
+
+</body>
+</html>
+
