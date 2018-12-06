fba4a6352f72ff764c0cfcaaacf544a54d8d59b0
FrankV <frank@sabertechnology.com>
2018-12-05 21:11:01 -0500

    Almost working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,83 @@
-<!doctype html>
-<html lang="en-us">
-
-<head>
-  <meta charset="UTF-8">
-  <title>Word Guess Game</title>
-  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-
-  <script defer src="assets/javascript/main.js"></script>
-</head>
-
-<body>
-        
 
 
-
-<script type="text/javascript">
-
-
-
-
-consol.log("this file works");
-
-
-</script>
-
-</body>
-</html>
+  function layoutLetters(winWordArr) {
+                
+                for (var i = 0; i < winWordArr.length; i++) {
+                  var targetDiv = document.getElementById("winWordDiv");
+  
+                  var newDiv = document.createElement("<div>");
+                                 
+                  newDiv.textContent = (" _ ");
+  
+                  targetDiv.appendChild(newDiv);
+  
+                  var setClassName = ("pos" + i);
+                 
+                  // newDiv.setAttribute("class", "letters"); // new class attribute
+                  // newDiv.setAttribute("class", newDiv.getAttribute("class") + setClassName); // append another class
+  
+                  //  newDiv.setAttribute("class", "letters");
+                  }
+              }
+                // newDiv.textContent = (winWordArr[i]);
+              
+              //Check if current userInput has already been tried
+  
+              function alreadyPressed(pressedKey, wasPressedArr) {
+                      var alreadyUsed = false;
+                      for (var i = 0; i < wasPressedArr.length; i++) {
+                          if (pressedKey == wasPressedArr[i]) {
+                              var alreadyUsed = true;
+                           console.log({
+                               alreadyUsed: alreadyUsed  });
+                          }
+                      }
+                          return alreadyUsed;
+              }
+              
+              //Check if current userInput is in the winning word
+              function checkWinWord(pressedKey, winWordLettersArr) {
+                  var inWinWord = false;
+              
+                  for (var i = 0; i < winWordLettersArr.length; i++) {
+                      if (pressedKey === winWordLettersArr[i]) {
+                          inWinWord = true;
+                      }
+                  }
+                  return inWinWord ;
+              }    
+              
+              //Tally the number of correctly guessed letters
+  
+              function tally(pressedKey, winWordLettersArr) {
+                 var tallyMatches = 0;
+                  for (var i = 0; i < winWordLettersArr.length; i++) {
+                      var setClassName = ("pos" + i);
+  
+                      console.log(setClassName);
+  
+                      if (pressedKey === winWordLettersArr[i]) {
+                      tallyMatches++;
+                          
+                       var targetDiv = document.getElementById("winWordDiv");
+  
+                      var newDiv = document.createElement("div");
+                   
+                      newDiv.textContent = (pressedKey);
+  
+                      targetDiv.appendChild(newDiv);
+  
+                      // targetDiv.innerHTML(newDiv);
+                      // document.getElementById("setClassName").innerHTML = pressedKey;
+  
+                      newDiv.setAttribute("class", "letters");
+  
+                      console.log ({
+                      tallyMatches: tallyMatches
+                      });
+  
+                      } 
+                  }
+                  return tallyMatches;
+              }
 
